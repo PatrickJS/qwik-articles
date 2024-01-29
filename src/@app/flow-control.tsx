aadababbcfa292ae25888072a442df8119ee8c5c
PatrickJS <github@patrickjs.com>
2024-01-29 11:57:43 -0800

    flow control

--- src/@app/flow-control.tsx
+++ src/@app/flow-control.tsx
@@ -1,17 +1,25 @@
-import { type JSXOutput, component$ } from "@builder.io/qwik";
+import { component$, type Signal, type JSXOutput } from "@builder.io/qwik";
 
 export function Show(props: {
   when: boolean;
+  "bind:when": Signal<boolean | undefined>;
   children?: JSXOutput;
-  else?: any;
+  else?: JSXOutput;
 }) {
+  if (props["bind:when"] !== undefined) {
+    return props["bind:when"].value ? props.children : props.else;
+  }
   return props.when ? props.children : props.else;
 }
 
-export function For(props: {
-  each?: any[];
-  children: (item: any, index: number, collection: any[]) => any;
+export function For<T>(props: {
+  each?: T[];
+  "bind:each"?: Signal<T[] | undefined>;
+  children: (item: T, index: number, collection: T[]) => JSXOutput;
 }) {
+  if (props["bind:each"] !== undefined) {
+    return props["bind:each"].value?.map(props.children);
+  }
   return props.each?.map(props.children);
 }
 
